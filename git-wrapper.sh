d0a4d061bdd0832fe7ceb1da9b1600fab8929956
Finn-Thorben Sell <dev@finn-thorben.me>
2022-07-20 20:30:31 +0200

    always execute git-crypt unlock

--- git-wrapper.sh
+++ git-wrapper.sh
@@ -2,7 +2,7 @@
 $(dirname $0)/git.bin "$@"
 EC=$?
 
-if [[ "$1" == "fetch" && -d ".git-crypt" ]]; then
+if [[ -d ".git-crypt" ]]; then
   export GNUPGHOME=/app/config/gpg/keys
   git-crypt unlock 2>/dev/null
 fi
