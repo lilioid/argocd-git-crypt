b5d27789ede0efc1f3e4eb99d14f17a38038079c
Finn-Thorben Sell <dev@finn-thorben.me>
2022-07-20 20:43:58 +0200

    fix git-wrapper to not unintentionally be recursive

--- git-wrapper.sh
+++ git-wrapper.sh
@@ -2,8 +2,9 @@
 $(dirname $0)/git.bin "$@"
 EC=$?
 
-if [ -d ".git-crypt" ]; then
+if [ "$1" = "checkout" -a -d ".git-crypt" -a ! "$GIT_CRYPT_RUNNING" = "true" ]; then
   export GNUPGHOME=/app/config/gpg/keys
+  export GIT_CRYPT_RUNNING=true
   git-crypt unlock > /dev/stderr
 fi
 
